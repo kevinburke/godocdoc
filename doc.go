3a7db4c1380346d7c62d4fede2c8a06f3245502c
Kevin Burke <kev@inburke.com>
2017-02-16 21:28:12 -0800

    Add Go documentation

---
+++ doc.go
@@ -0,0 +1,11 @@
+// godocdoc is a tool for browsing Go documentation.
+//
+// Usage:
+//
+// 	godocdoc [-port PORT]
+//
+// This will start the godoc binary, wait for it to listen on the specified
+// port, and when it is listening, open a browser tab to the package you are
+// currently looking at. It's useful for opening the documentation for the
+// project you're currently examining in the terminal.
+package main
